2d1a192d082c5a6819f8f5168cee464548162285
Noxxxelor <stu2301321031@uni-plovdiv.bg>
2025-05-08 00:44:35 +0300

    Created entities, connected database, created base repository

---
+++ course-work/Implementations/IntershipApp/IntershipApp/Common/Entities/Application.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Entities
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; } // Навигационное свойство
+
+        public int InternshipId { get; set; }
+        public Internship Internship { get; set; } // Навигационное свойство
+
+        public DateTime ApplicationDate { get; set; } // Дата подачи заявки
+        public ApplicationStatus Status { get; set; } // Статус заявки
+    }
+
+    public enum ApplicationStatus
+    {
+        Pending,    // Ожидает рассмотрения
+        Accepted,   // Принята
+        Rejected    // Отклонена
+    }
+}
+
---
+++ course-work/Implementations/IntershipApp/IntershipApp/Common/Entities/Intership.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Common.Entities
+{
+    public class Internship
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company Company { get; set; } = null!;
+
+        public ICollection<Application> Applications { get; set; } = new List<Application>();
+    }
+}
+
